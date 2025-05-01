1ae9bcecc6ab54b1438af11a577da974639d5c25
SamridhiShridhar <122141988+SamridhiShridhar@users.noreply.github.com>
2025-05-01 15:44:41 +0530

    Delete UrbanMart-Backend directory

--- UrbanMart-Backend/models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategorySchema = new mongoose.Schema({
-  category_name: { type: String, required: true },
-  description: { type: String },
-  imageName: { type: String }
-});
-
-module.exports = mongoose.model('Category', CategorySchema);
--- UrbanMart-Backend/models/DeliveryPersonnel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DeliveryPersonnelSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phone: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('DeliveryPersonnel', DeliveryPersonnelSchema);
